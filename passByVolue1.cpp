72b33b5b3a1f408d8512a432dee1596e83b63527
Sima-Kumari <simak4484@gmail.com>
2026-06-07 23:58:38 +0530

    Add files via upload

---
+++ passByVolue1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void changeX(int x) {
+    x = 2*x;
+    cout << "x = " << x <<endl;
+}
+
+int main() {
+    int x = 5;
+    changeX(x);
+
+    cout << "x = " << x << endl;
+
+    return 0;
+}
